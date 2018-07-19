3e270cad2e6ab93d75a73b0964bb18e11c616aac
jaethao <mjathao@gmail.com>
2018-07-19 13:27:01 -0500

    added logic

--- public/assets/logic/logic.js
+++ public/assets/logic/logic.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
 
+    // hide sign up form
     $("#signUpForm").hide();
-    
+
     //display sign up form when sign up button clicked
-    $("#signBtn").click(function() {
+    $("#signBtn").click(function(){
       $("#loginForm").fadeOut();
       $("#signUpForm").fadeIn();
-
     });
 
 });
