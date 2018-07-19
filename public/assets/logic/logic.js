61b0259a71231729e9b029ebe8def0ba5ce7afc5
jaethao <mjathao@gmail.com>
2018-07-19 13:09:42 -0500

    create forms

--- public/assets/logic/logic.js
+++ public/assets/logic/logic.js
@@ -1,1 +1,12 @@
-console.log("connected!");
+$( document ).ready(function() {
+
+    $("#signUpForm").hide();
+    
+    //display sign up form when sign up button clicked
+    $("#signBtn").click(function() {
+      $("#loginForm").fadeOut();
+      $("#signUpForm").fadeIn();
+
+    });
+
+});
